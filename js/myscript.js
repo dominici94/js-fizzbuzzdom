91931de0aa69169d4c6db5dc7b79f71da01d4209
dominici94 <mattiadominici94@gmail.com>
2021-11-09 15:01:10 +0100

    MILESTONE 1

--- js/myscript.js
+++ js/myscript.js
@@ -10,6 +10,15 @@
 for(let i=1; i <= 100; i++){
 
     const number = i;
-    console.log(i);
+    
+    if( (number % 3 == 0) &&(number % 5 == 0) ){
+        console.log('FizzBuzz');
+    }else if( number % 3 == 0){
+        console.log('Fizz');
+    }else if( number % 5 == 0){
+        console.log('Buzz');
+    }else{
+        console.log(i);
+    }
 
-}
+};
